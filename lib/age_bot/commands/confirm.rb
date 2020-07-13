14add0602442c68045ced9d7758f9c02e81fc66a
Ken Spencer <me@iotaspencer.me>
2020-07-13 16:42:29 -0400

    add a command to have a on-command message for older users that exist before the bot

--- lib/age_bot/commands/confirm.rb
+++ lib/age_bot/commands/confirm.rb
@@ -7,6 +7,18 @@ module AgeBot
       module ConfirmAge
         HELPERS = AgeBot::Bot::Helpers
         extend Discordrb::Commands::CommandContainer
+        command(:hello) do |event|
+          user = event.user
+          user.pm(<<~HERE)
+            Hello, #{user.name}, in order to post or read #{event.server.name} messages you must be a certain role as well as submitted a form of ID with the server in question.
+            For #{event.server.name} that role is **#{event.server.role(AgeBot::Configs::ServerDB.db.servers[event.server.id.to_s].role).name}**
+            To do so, please send me a picture of your ID with everything but your 'date of birth' blacked out along with some sort of Discord™ proof (Your account page with the email blacked out or a handwritten Discord™ tag 
+              
+              For you that would be `#{event.user.distinct}`
+              When you do so, attach this message to the picture as a caption
+              `$verify #{event.server.id}`
+          HERE
+        end
         command(:confirm, help_available: false, min_args: 2) do |event, user, message_id|
           admin        = event.server.member(event.user.id)
           user_name    = user.split('#')[0]
