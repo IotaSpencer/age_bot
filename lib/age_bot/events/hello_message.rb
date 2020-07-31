301a6c8975d5a3c65ad64bf92241d70901bcc2d4
Ken Spencer <me@iotaspencer.me>
2020-07-31 16:13:52 -0400

    parse mentions

---
+++ lib/age_bot/events/hello_message.rb
@@ -0,0 +1,25 @@
+module AgeBot
+  module Bot
+    module DiscordEvents
+      module MemberJoin
+        extend Discordrb::EventContainer
+        message do |event|
+          if event.channel.name =~ /\#hello/
+            puts Discordrb.parse_mention(event.content).any? { |obj| obj == event.channel }
+            user = event.author
+            user.pm(<<~HERE)
+              Hello, #{user.name}, in order to post or read #{event.server.name} messages you must be a certain role as well as submitted a form of ID with the server in question.
+              For #{event.server.name} that role is **#{event.server.role(AgeBot::Configs::ServerDB.db.servers[event.server.id.to_s].role).name}**
+              To do so, please send me a picture of your ID with everything but your 'date of birth' blacked out along with some sort of Discord™ proof (Your account page with the email blacked out or a handwritten Discord™ tag 
+                
+                For you that would be `#{event.user.distinct}`
+                When you do so, attach this message to the picture as a caption
+                `#{AgeBot::Configs::BotConfig.config.bot.prefix}verify #{event.server.id}`
+            HERE
+            
+          end
+        end
+      end
+    end
+  end
+end
