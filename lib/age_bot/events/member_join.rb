68d10e192264ea8c5bd6ba3c6aaf4f9be0d2983c
Ken Spencer <me@iotaspencer.me>
2020-09-08 04:06:10 -0400

    explain a bit more on verification

--- lib/age_bot/events/member_join.rb
+++ lib/age_bot/events/member_join.rb
@@ -8,7 +8,11 @@ module AgeBot
           user.pm(<<~HERE )
             Hello, #{user.name}, in order to post or read #{event.server.name} messages you must be a certain role as well as submitted a form of ID with the server in question.
             For #{event.server.name} that role is **#{event.server.role(AgeBot::Configs::ServerDB.db.servers[event.server.id.to_s].role).name}**
-            To do so, please send me a picture of your ID with everything but your 'date of birth' blacked out along with some sort of Discord™ proof (Your account page with the email blacked out or a handwritten Discord™ tag 
+            To do so, please send me a picture of your ID with everything but your 'date of birth' blacked out.
+            You must also include some sort of Discord™ proof of identity (Your account page with the email blacked out or a handwritten Discord™ tag 
+            These **must** be in the **same shot** otherwise you will be rejected.
+
+            See #id-example on the server for possible ID solutions. 
               
               For you that would be `#{event.user.distinct}`
               When you do so, attach this message to the picture as a caption
