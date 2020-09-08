15f0fd3f173fee187f5c32d99900e74017ae03b4
Ken Spencer <me@iotaspencer.me>
2020-09-08 03:57:14 -0400

    add logging functions

--- lib/age_bot/commands/fcom.rb
+++ lib/age_bot/commands/fcom.rb
@@ -12,8 +12,13 @@ module AgeBot
             description: "Force a #{AgeBot::Configs::BotConfig.config.bot.prefix}hello on a user",
             usage:       "#{AgeBot::Configs::BotConfig.config.bot.prefix}fhello"
         }) do |event, username|
+          Logger.debug 'Requesting Chunks'
           event.bot.request_chunks(event.server.id.to_s)
+          Logger.debug 'Got Chunks'
+          Logger.debug 'Parsing Tag'
           user = AgeBot::Bot::Helpers.member_from_tag(event.server.id.to_s, username)
+          Logger.debug 'Tag parsed.'
+          Logger.debug 'DM\'ing user'
           user.dm(<<~HEREDOC)
             Hello, #{user.name}, in order to post or read #{event.server.name} messages you must be a certain role as well as submitted a form of ID with the server in question.
             For #{event.server.name} that role is **#{event.server.role(AgeBot::Configs::ServerDB.db.servers[event.server.id.to_s].role).name}**
@@ -23,7 +28,7 @@ module AgeBot
             When you do so, attach this message to the picture as a caption.
             `#{AgeBot::Configs::BotConfig.config.bot.prefix}verify #{event.server.id}`
           HEREDOC
-
+          Logger.debug "DM'd user"
         end
       end
     end
