4c925e3c4375bfeaa43729ba88858d6c9c8cc330
Ken Spencer <me@iotaspencer.me>
2020-09-26 17:11:04 -0400

    make a better scheduler

--- lib/age_bot/scheduler.rb
+++ lib/age_bot/scheduler.rb
@@ -1,15 +1,36 @@
 require 'rufus-scheduler'
-
+require 'ostruct'
 module AgeBot
 
   module Bot
+    # Instances of Rufus::Scheduler
+    class Schedules
+      @schedules = {}
+      # @return [Hash<String => Rufus::Scheduler>]
+      def self.schedules
+        @schedules
+      end
 
-    class Scheduler
-      @scheduler = Rufus::Scheduler.new
+      # @return [Rufus::Scheduler]
+      # @param [String,Symbol] name name of scheduler
+      def self.[](name)
+        @schedules.fetch(name, nil)
+      end
 
-      def self.scheduler
-        @scheduler
+      # Remove a scheduler from the 'list'
+      # @param [String,Symbol] name name of scheduler
+      def self.delete(name)
+        @schedules.delete(name)
+      end
+
+      # Add a scheduler to the 'list'
+      # @return [Rufus::Scheduler]
+      # @param [String,Symbol] name name of scheduler
+      def self.add(name)
+        scheduler = Rufus::Scheduler.new
+        @schedules.store(name, scheduler)
+        scheduler
       end
     end
   end
-end 
+end
