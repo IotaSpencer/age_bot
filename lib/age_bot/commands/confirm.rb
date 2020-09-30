b8ab8ec16751a6ac5ab1092e6ba664e59e27ca7e
Ken Spencer <me@iotaspencer.me>
2020-09-30 10:33:00 -0400

    rescue out if we get a NoPermission Error

--- lib/age_bot/commands/confirm.rb
+++ lib/age_bot/commands/confirm.rb
@@ -16,16 +16,22 @@ module AgeBot
           if event.channel.name =~ /hello/
             event.bot.request_chunks(event.server.id.to_s)
             user = event.user
-            Logger.debug "#{event.user.distinct} triggered '&hello' via '#{event.channel}'"
-            user.pm(<<~HERE)
-              Hello, #{user.name}, in order to post or read #{event.server.name} messages you must be a certain role as well as submitted a form of ID with the server in question.
-              For #{event.server.name} that role is **#{event.server.role(AgeBot::Configs::ServerDB.db.servers[event.server.id.to_s].role).name}**
-              To do so, please send me a picture of your ID with everything but your 'date of birth' blacked out along with some sort of Discord™ proof (Your account page with the email blacked out or a handwritten Discord™ tag 
-                
-                For you that would be `#{event.user.distinct}`
-                When you do so, attach this message to the picture as a caption
-                `#{AgeBot::Configs::BotConfig.config.bot.prefix}verify #{event.server.id}`
-            HERE
+            Logger.debug "#{event.user.distinct} triggered '&hello' via '#{event.channel.name}' on '#{event.server.name}'"
+            begin
+              user.pm(<<~HERE)
+                Hello, #{user.name}, in order to post or read #{event.server.name} messages you must be a certain role as well as submitted a form of ID with the server in question.
+                For #{event.server.name} that role is **#{event.server.role(AgeBot::Configs::ServerDB.db.servers[event.server.id.to_s].role).name}**
+                To do so, please send me a picture of your ID with everything but your 'date of birth' blacked out along with some sort of Discord™ proof (Your account page with the email blacked out or a handwritten Discord™ tag) 
+                  
+                  For you that would be `#{event.user.distinct}`
+                  When you do so, attach this string below to the picture as a caption.
+
+                  `#{AgeBot::Configs::BotConfig.config.bot.prefix}verify #{event.server.id}`
+              HERE
+            rescue Discordrb::Errors::NoPermission
+              Logger.debug "I've received a NoPermission Error, either I'm blocked or something weird is going on."
+            end
+
             sleep(5)
             event.message.delete
           end
