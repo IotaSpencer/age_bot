11e113fcd19172a9f7aafc070d754be61cf58f59
Ken Spencer <me@iotaspencer.me>
2020-10-16 18:28:09 -0400

    check for no permission
    (cherry picked from commit 2851b55773647438219e83d65be568c6e2bf06b0)

--- lib/age_bot/events/member_bad_hello.rb
+++ lib/age_bot/events/member_bad_hello.rb
@@ -11,39 +11,47 @@ module AgeBot
             if AgeBot::Bot::Helpers.parse_mention(event.content, event.server, event: event)&.name =~ /\#?hello/
               # message was '#hello' or mention of #hello
               user = event.author
-              user.pm(<<~HERE)
-                Hello, #{user.name}, in order to post or read #{event.server.name} messages you must be a certain role as well as submitted a form of ID with the server in question.
-                For #{event.server.name} that role is **#{event.server.role(AgeBot::Configs::ServerDB.db.servers[event.server.id.to_s].role).name}**
-                To do so, please send me a picture of your ID with everything but your 'date of birth' blacked out along with some sort of Discord™ proof (Your account page with the email blacked out or a handwritten Discord™ tag 
-                  
-                  For you that would be `#{event.user.distinct}`
-                  When you do so, attach this message to the picture as a caption
-                  `#{AgeBot::Configs::BotConfig.config.bot.prefix}verify #{event.server.id}`
-                
-                You are receiving this message because you messaged ##{event.channel.name} a message that triggered me.
-                Your message will now be deleted since the message holds no purpose in ##{event.channel.name}.
+              begin
+                user.pm(<<~HERE)
+                  Hello, #{user.name}, in order to post or read #{event.server.name} messages you must be a certain role as well as submitted a form of ID with the server in question.
+                  For #{event.server.name} that role is **#{event.server.role(AgeBot::Configs::ServerDB.db.servers[event.server.id.to_s].role).name}**
+                  To do so, please send me a picture of your ID with everything but your 'date of birth' blacked out along with some sort of Discord™ proof (Your account page with the email blacked out or a handwritten Discord™ tag 
+                    
+                    For you that would be `#{event.user.distinct}`
+                    When you do so, attach this message to the picture as a caption
+                    `#{AgeBot::Configs::BotConfig.config.bot.prefix}verify #{event.server.id}`
 
-                You may ask questions about the process in ##{event.channel.name} but other than that, non-complying questions or messages will be deleted.
-              HERE
+                  You are receiving this message because you messaged ##{event.channel.name} a message that triggered me.
+                  Your message will now be deleted since the message holds no purpose in ##{event.channel.name}.
+
+                  You may ask questions about the process in ##{event.channel.name} but other than that, non-complying questions or messages will be deleted.
+                HERE
+              rescue Discordrb::Errors::NoPermission
+                Logger.debug "I've received a NoPermission Error, either I'm blocked or something weird is going on."
+              end
               event.channel.delete_message(event.message.id)
             elsif event.content =~ /^[Hh][Ee][Ll][Ll][Oo]$/
               # message was 'hello' or a variation on that
               user = event.author
+              begin
               user.pm(<<~HERE)
-                Hello, #{user.name}, in order to post or read #{event.server.name} messages you must be a certain role as well as submitted a form of ID with the server in question.
-                For #{event.server.name} that role is **#{event.server.role(AgeBot::Configs::ServerDB.db.servers[event.server.id.to_s].role).name}**
-                To do so, please send me a picture of your ID with everything but your 'date of birth' blacked out along with some sort of Discord™ proof (Your account page with the email blacked out or a handwritten Discord™ tag 
-                  
-                  For you that would be `#{event.user.distinct}`
-                  When you do so, attach this message to the picture as a caption
-                  `#{AgeBot::Configs::BotConfig.config.bot.prefix}verify #{event.server.id}`
-                
-                You are receiving this message because you messaged #{event.channel.name} a message that triggered me.
-                Your message will now be deleted since the message holds no purpose in #{event.channel.name}.
+                  Hello, #{user.name}, in order to post or read #{event.server.name} messages you must be a certain role as well as submitted a form of ID with the server in question.
+                  For #{event.server.name} that role is **#{event.server.role(AgeBot::Configs::ServerDB.db.servers[event.server.id.to_s].role).name}**
+                  To do so, please send me a picture of your ID with everything but your 'date of birth' blacked out along with some sort of Discord™ proof (Your account page with the email blacked out or a handwritten Discord™ tag 
+                    
+                    For you that would be `#{event.user.distinct}`
+                    When you do so, attach this message to the picture as a caption
+                    `#{AgeBot::Configs::BotConfig.config.bot.prefix}verify #{event.server.id}`
 
-                You may ask questions about the process in #{event.channel.name} but other than that, non-complying questions or messages will be deleted.
-              HERE
-              event.channel.delete_message(event.message.id)
+                  You are receiving this message because you messaged #{event.channel.name} a message that triggered me.
+                  Your message will now be deleted since the message holds no purpose in #{event.channel.name}.
+
+                  You may ask questions about the process in #{event.channel.name} but other than that, non-complying questions or messages will be deleted.
+                HERE
+                event.channel.delete_message(event.message.id)
+              rescue Discordrb::Errors::NoPermission
+                Logger.debug "I've received a NoPermission Error, either I'm blocked or something weird is going on."
+              end
             end
           end
         end
--- lib/age_bot/events/member_join.rb
+++ lib/age_bot/events/member_join.rb
@@ -5,19 +5,24 @@ module AgeBot
         extend Discordrb::EventContainer
         member_join do |event|
           user = event.user
-          user.pm(<<~HERE )
-            Hello, #{user.name}, in order to post or read #{event.server.name} messages you must be a certain role as well as submitted a form of ID with the server in question.
-            For #{event.server.name} that role is **#{event.server.role(AgeBot::Configs::ServerDB.db.servers[event.server.id.to_s].role).name}**
-            To do so, please send me a picture of your ID with everything but your 'date of birth' blacked out.
-            You must also include some sort of Discord™ proof of identity (Your account page with the email blacked out or a handwritten Discord™ tag 
-            These **must** be in the **same shot** otherwise you will be rejected.
+          sleep 5
+          begin
+            user.pm(<<~HERE)
+              Hello, #{user.name}, in order to post or read #{event.server.name} messages you must be a certain role as well as submitted a form of ID with the server in question.
+              For #{event.server.name} that role is **#{event.server.role(AgeBot::Configs::ServerDB.db.servers[event.server.id.to_s].role).name}**
+              To do so, please send me a picture of your ID with everything but your 'date of birth' blacked out.
+              You must also include some sort of Discord™ proof of identity (Your account page with the email blacked out or a handwritten Discord™ tag 
+              These **must** be in the **same shot** otherwise you will be rejected.
 
-            See #id-example on the server for possible ID solutions. 
-              
-              For you that would be `#{event.user.distinct}`
-              When you do so, attach this message to the picture as a caption
-              `#{AgeBot::Configs::BotConfig.config.bot.prefix}verify #{event.server.id}`
-          HERE
+              See #id-example on the server for possible ID solutions. 
+                
+                For you that would be `#{event.user.distinct}`
+                When you do so, attach this message to the picture as a caption
+                `#{AgeBot::Configs::BotConfig.config.bot.prefix}verify #{event.server.id}`
+            HERE
+          rescue Discordrb::Errors::NoPermission
+            Logger.debug "I've received a NoPermission Error from a join, either I'm blocked or something weird is going on."
+          end
         end
       end
     end
