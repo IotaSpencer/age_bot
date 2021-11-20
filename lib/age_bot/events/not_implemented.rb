f0e13dbe25874c36bf7060fb4fa611b55ae9578f
IotaSpencer <me@iotaspencer.me>
2021-11-20 02:18:15 -0500

    not_implemented.rb: actually include the scenario were testing for

--- lib/age_bot/events/not_implemented.rb
+++ lib/age_bot/events/not_implemented.rb
@@ -11,11 +11,11 @@ module AgeBot
           user = event.user
           msg_obj = event.message.content.split(' ')
           msg = msg_obj
+          channel = event.bot.parse_mention(msg, event.server&.id ? event.server.id : nil) || nil
 
           case msg
           when /^(!hello|& hello)(.*)?/
 
-            channel = event.bot.parse_mention(msg, event.server&.id ? event.server.id : nil)
             dm_indi = event.server.nil?
             if dm_indi == true
               event.respond("Its &hello in #hello\nOn the server you have in common with me, if you haven't already done this before for that server.")
@@ -29,7 +29,11 @@ module AgeBot
             dm_indi = event.server.nil?
             if dm_indi == true
               event.respond("")
+            else
+              event.respond("#{user.mention}: You do that in a private message to the bot, not to this channel.")
+              !event.message.attachments.empty? ? event.respond("Also do not send your ID+tag to this public channel") : nil
             end
+            when !channel.nil?
           end
 
         end
