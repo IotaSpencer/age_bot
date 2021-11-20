6510136ebc58deee6d6fdcd02103702a0925c94b
Ken Spencer <me@iotaspencer.me>
2021-11-20 07:51:40 -0500

    updates people updates

--- lib/age_bot/events/not_implemented.rb
+++ lib/age_bot/events/not_implemented.rb
@@ -13,27 +13,30 @@ module AgeBot
           msg = msg_obj
           channel = event.bot.parse_mention(msg, event.server&.id ? event.server.id : nil) || nil
 
-          case msg
-          when /^(!hello|& hello)(.*)?/
+          if msg
+            if msg =~ /^(!hello|& hello)(.*)?/
 
-            dm_indi = event.server.nil?
-            if dm_indi == true
-              event.respond("Its &hello in #hello\nOn the server you have in common with me, if you haven't already done this before for that server.")
+              dm_indi = event.server.nil?
+              if dm_indi == true
+                event.respond("Its &hello in #hello\nOn the server you have in common with me, if you haven't already done this before for that server.")
+              else
+                event.respond("#{user.mention}: Its &hello in #hello, nothing else.")
+              end
+              sleep(60)
+              event.message.delete
+            elsif msg =~ /^([!&@$]verify)(.*)/
+              channel = event.channel
+              dm_indi = event.server.nil?
+              if dm_indi == true
+                nil
+              else
+                event.respond("#{user.mention}: You do that in a private message to the bot, not to this channel.")
+                !event.message.attachments.empty? ? event.respond("Also do not send your ID+tag to this public channel") : nil
+              end
+              if !channel.nil?
+              end
             else
-              event.respond("#{user.mention}: Its &hello in #hello, nothing else.")
             end
-            sleep(60)
-            event.message.delete
-          when /^([!&@$]verify)(.*)/
-            channel = event.channel
-            dm_indi = event.server.nil?
-            if dm_indi == true
-              nil
-            else
-              event.respond("#{user.mention}: You do that in a private message to the bot, not to this channel.")
-              !event.message.attachments.empty? ? event.respond("Also do not send your ID+tag to this public channel") : nil
-            end
-            when !channel.nil?
           end
         end
       end
